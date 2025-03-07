c95f7432c6dc41c367158a670aefa67758ccdf55
DeydanhH <devdey146@gmail.com>
2025-03-06 23:56:48 -0600

    Add files via upload

---
+++ me.js
@@ -0,0 +1,12 @@
+// Deydanh Hackett 3-6-2025
+"use strict";
+
+let name = "Deydanh Hackett";
+let favoriteColor = "Purple";
+let hobby = "Playing Basketball";
+let favoriteFood = "Chicken Alfredo";
+
+console.log(name);
+console.log(favoriteColor);
+console.log(hobby);
+console.log(favoriteFood);
